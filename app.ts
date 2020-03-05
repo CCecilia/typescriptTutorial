51feca741c08076392907d592347e312af7d4f61
Christian Cecilia <christian.cecilia1@gmail.com>
2020-03-04 21:20:27 -0500

    init

---
+++ app.ts
@@ -0,0 +1,12 @@
+console.log('test');
+// Var Declaration
+let test: string = 'hello'
+let boolVar: boolean = true
+
+let numberVar_big: BigInteger = 678678
+let numberVar: Number = 45
+
+numberVar_big
+test
+
+// Arrays
